76d75ea92f29d63b6c4a7261799bde4ad76ffd90
fetthesten <coercion@online.no>
2018-11-01 15:05:07 +0100

    converted the game to 3d
    because why not

--- scr/player_test.gd
+++ scr/player_test.gd
@@ -1,21 +1,16 @@
-extends KinematicBody2D
+extends KinematicBody
 
 export var movement_speed = 6000
-onready var area_check = $Area2D
+export var camera_offset = Vector3(0,10,15)
+onready var area_check = $area_check
 onready var camera = main.current_camera
 onready var info_label = main.info_label
 
-const V2_ZERO = Vector2(0,0)
-const V2_UP = Vector2(0,-1)
-const V2_DOWN = Vector2(0,1)
-const V2_LEFT = Vector2(-1,0)
-const V2_RIGHT = Vector2(1,0)
-
 var weapons = []
 var current_weapon = 0
-var last_dir = V2_UP
-var weap_img
-var weapon_offset = V2_ZERO
+var weapon_model
+var last_dir = main.V3_UP
+onready var weapon_offset = $weapon_offset
 var weap_tween
 var can_move = true
 
@@ -25,10 +20,9 @@ var camera_target = self
 func _ready():
 	weap_tween = Tween.new()
 	add_child(weap_tween)
-	weapon_offset = $sorter/Sprite.texture.get_size() / 2
 	
-	weapons.append({'name': 'knife', 'damage': 1, 'scale': 1.0, 'distance': 8, 'cooldown': Timer.new(), 'movestop': Timer.new(), 'ingame_img': preload('res://gfx/ingame/knife01.png'), 'hud_img': preload('res://gfx/hud/knife01.png')})
-	weapons.append({'name': 'sword', 'damage': 3, 'scale': 1.0, 'distance': 16, 'cooldown': Timer.new(), 'movestop': Timer.new(), 'ingame_img': preload('res://gfx/ingame/sword01.png'),  'hud_img': preload('res://gfx/hud/sword01.png')})
+	weapons.append({'name': 'knife', 'damage': 1, 'scale': 1.0, 'distance': 8, 'cooldown': Timer.new(), 'movestop': Timer.new(), 'ingame_mdl': load('res://obj/mdl/weapons/knife01.tscn').instance(), 'hud_img': preload('res://gfx/hud/knife01.png')})
+	weapons.append({'name': 'sword', 'damage': 3, 'scale': 1.0, 'distance': 16, 'cooldown': Timer.new(), 'movestop': Timer.new(), 'ingame_mdl': load('res://obj/mdl/weapons/sword01.tscn').instance(),  'hud_img': preload('res://gfx/hud/sword01.png')})
 	weapons[0].cooldown.one_shot = true
 	weapons[0].cooldown.wait_time = 0.3
 	weapons[0].cooldown.connect('timeout', self, 'attack_stop')
@@ -37,6 +31,8 @@ func _ready():
 	weapons[0].movestop.wait_time = 0.001
 	weapons[0].movestop.connect('timeout', self, 'move_restore')
 	add_child(weapons[0].movestop)
+	add_child(weapons[0].ingame_mdl)
+	weapons[0].ingame_mdl.visible = false
 	weapons[1].cooldown.one_shot = true
 	weapons[1].cooldown.wait_time = 0.5
 	weapons[1].cooldown.connect('timeout', self, 'attack_stop')
@@ -45,35 +41,35 @@ func _ready():
 	weapons[1].movestop.wait_time = 0.15
 	weapons[1].movestop.connect('timeout', self, 'move_restore')
 	add_child(weapons[1].movestop)
-
-	weap_img = Sprite.new()
-	weap_img.visible = false
-	weap_img.centered = false
-	$sorter.add_child(weap_img)
+	add_child(weapons[1].ingame_mdl)
+	weapons[1].ingame_mdl.visible = false
+	
+	weapon_model = weapons[0].ingame_mdl
+	
 	change_weapon(true)
 	
 func _process(delta):
-	var move = V2_ZERO
+	var move = main.V3_ZERO
 	if Input.is_action_pressed('game_up'):
-		move += V2_UP
+		move += main.V3_UP
 	elif Input.is_action_pressed('game_down'):
-		move += V2_DOWN
+		move += main.V3_DOWN
 	if Input.is_action_pressed('game_left'):
-		move += V2_LEFT
+		move += main.V3_LEFT
 	elif Input.is_action_pressed('game_right'):
-		move += V2_RIGHT
+		move += main.V3_RIGHT
 	
 	if Input.is_action_just_pressed('game_change_weapon'):
 		change_weapon()
 	
-	if move != V2_ZERO:
+	if move != main.V3_ZERO:
 		last_dir = move
 	
 	if Input.is_action_pressed('game_attack'):
 		attack_start()
 	
 	if not can_move:
-		move = V2_ZERO
+		move = main.V3_ZERO
 	
 	move = move.normalized()
 	
@@ -82,7 +78,10 @@ func _process(delta):
 	move_and_slide(move * movement_speed * delta)
 	if camera == null:
 		camera = main.get_camera()
-	camera.position = camera_target.transform.origin
+	camera.transform.origin = camera_target.transform.origin + camera_offset
+	camera.rotation_degrees = Vector3(-45, 0,0)
+	
+	weapon_model.transform.origin = weapon_offset.transform.origin
 	
 	info_label.text = 'current area: ' + current_area
 	info_label.text += '\nweapon: ' + weapons[current_weapon].name
@@ -92,15 +91,9 @@ func attack_start():
 	if weapons[current_weapon].cooldown.is_stopped():
 		var weapon = weapons[current_weapon]
 		weapon.cooldown.start()
-		weap_img.visible = true
-		weap_img.set_scale(Vector2(weapon.scale, weapon.scale))
-		var offset = last_dir.normalized() * weapon_offset
-		weap_img.global_position = global_position + offset
-		weap_img.look_at(position + (last_dir * 100))
-		var tween_target = (weap_img.global_position + offset)
-		#tween_path *= 1.05
-		weap_tween.targeting_property(weap_img, 'global_position', weap_img, 'global_position', tween_target, weapon.cooldown.time_left, Tween.TRANS_LINEAR, Tween.EASE_IN)
-		weap_tween.start()
+		
+		#weap_tween.targeting_property(weap_img, 'global_position', weap_img, 'global_position', tween_target, weapon.cooldown.time_left, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		#weap_tween.start()
 		can_move = false
 		weapon.movestop.start()
 
@@ -108,7 +101,8 @@ func move_restore():
 	can_move = true
 
 func attack_stop():
-	weap_img.visible = false
+	#weap_img.visible = false
+	pass
 
 func check_areas(current_areas):
 	for area in current_areas:
@@ -129,6 +123,7 @@ func change_weapon(init = false):
 		current_weapon += 1
 		if current_weapon >= weapons.size():
 			current_weapon = 0
-	weap_img.texture = weapons[current_weapon].ingame_img
-	weap_img.offset = Vector2(0, -(weap_img.texture.get_size().y * 0.5))
+	weapon_model.visible = false
+	weapon_model = weapons[current_weapon].ingame_mdl
+	weapon_model.visible = true
 	main.hud_weapon.texture = weapons[current_weapon].hud_img
